2fcffbd15f402db645eb573b85b454d847063ed5
Jarrod Jeffrey Ingram <jing0012@uni.sydney.edu.au>
2014-08-11 13:01:05 +1000

    Muh tweaks

--- dot/hydra/init.lua
+++ dot/hydra/init.lua
@@ -29,6 +29,7 @@ hotkey.bind(mod, "F", win.max)
 hotkey.bind(mod, "Q", function() application.launchorfocus("iTerm") end)
 hotkey.bind(mod, "W", function() application.launchorfocus("Google Chrome") end)
 hotkey.bind(mod, "E", function() application.launchorfocus("Finder") end)
+hotkey.bind(mod, "R", function() application.launchorfocus("Preview") end)
 
 -- Updates.
 timer.new(timer.weeks(1), checkforupdates):start()
