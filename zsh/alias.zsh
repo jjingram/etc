447641f10ee6a8c0d54c53c556b5df8fa27ab968
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-06-28 14:59:07 +1000

    Bash finally got its shit together in 4.3 and NeoVim is promising.

--- zsh/alias.zsh
+++
@@ -1,8 +0,0 @@
-alias q='exit'
-alias c='clear'
-alias g='git'
-alias j='jobs'
-if [ $(uname) = 'Darwin' ]; then
-	export EDITOR='mvim -v'
-fi
-alias e=$EDITOR
--- zsh/key.zsh
+++
@@ -1,2 +0,0 @@
-bindkey -M vicmd 'j' down-line-or-search
-bindkey -M vicmd 'k' up-line-or-search
