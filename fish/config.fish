09454ac06886d7198db82b2a4cf47b40fb621960
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-06-29 18:23:04 +1000

    Back and forth. Fish is fantastic though, I believe I'll keep it.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,23 @@
-set -U EDITOR nvim
+set -U EDITOR mvim -v
+set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+# Texlive
+set PATH $PATH /usr/texbin
+# Go
+set GOPATH $HOME/go
+set PATH $PATH $GOPATH/bin /usr/local/opt/go/libexec/bin
+# Cabal
+set PATH $PATH $HOME/.cabal/bin
+
+alias e='mvim -v'
+alias q='exit'
+alias c='clear'
+alias j='jobs'
 
 fish_vi_mode
 
 # Base16 Shell colours
 eval sh ~/etc/color/base16-shell/base16-google.light.sh
+
+if not set -q TMUX
+    exec tmux
+end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,10 +2,10 @@
 set __fish_git_prompt_color_branch magenta
 
 function fish_prompt
-        set last_status $status
-        set_color $fish_color_cwd
-        printf '%s' (prompt_pwd)
-        set_color normal
-        printf '%s ' (__fish_git_prompt)
-        set_color normal
+    set last_status $status
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
+    printf '%s ' (__fish_git_prompt)
+    set_color normal
 end
