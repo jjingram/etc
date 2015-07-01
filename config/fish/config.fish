b220fe376fb62f0663edbae1316634a403707a65
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-07-02 08:29:53 +1000

    Cabal options, args can be given to homebrew installs, mucking around with some Shougo vim plugins.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,14 @@
-set -U EDITOR mvim -v
+set -U EDITOR vim
 set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
-set PATH $PATH ~/bin
 # Texlive
 set PATH $PATH /usr/texbin
 # Go
 set GOPATH $HOME/go
 set PATH $PATH $GOPATH/bin /usr/local/opt/go/libexec/bin
 # Haskell
+set PATH $PATH $HOME/.cabal/bin
 
-alias e='mvim -v'
+alias e='vim'
 alias q='exit'
 alias c='clear'
 alias j='jobs'
