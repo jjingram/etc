057ecc87b10ff568a8900b7340857ffe6b0edb49
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-07-02 10:21:07 +1000

    Better fish script support in vim. Should look at combinding the functionality of the previous one with this one.

--- config/fish/functions/extract.fish
+++ config/fish/functions/extract.fish
@@ -1,21 +1,16 @@
 function extract --description "Expand or extract bundled & compressed files"
     set --local ext (echo $argv[1] | awk -F. '{print $NF}')
     switch $ext
-    # Non-compressed, just bundled.
     case tar
         tar -xvf $argv[1]
     case gz
-        # tar bundle compressed with gzip.
         if test (echo $argv[1] | awk -F. '{print $(NF-1)}') = tar
             tar -zxvf $argv[1]
-        # Single gzip.
         else
             gunzip $argv[1]
         end
-    # Same as tar.gz.
     case tgz
         tar -zxvf $argv[1]
-    # tar compressed with bzip2.
     case bz2
         tar -jxvf $argv[1]
     case rar
