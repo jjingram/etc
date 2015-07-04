913a64f0edea23fd4acf7e2e6affd62a9a676f25
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-07-04 20:04:54 +1000

    Bash is great when it doesn't do that history-search bug.

--- config/fish/functions/fish_prompt.fish
+++
@@ -1,5 +0,0 @@
-function fish_prompt --description 'Write out the left prompt'
-    set_color $fish_color_cwd
-    printf "%s " (prompt_pwd)
-    set_color normal
-end
--- config/fish/functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls --description 'List contents of directory'
-	command ls -hFG $argv
-end
